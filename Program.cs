d64daaede7b8936e951dd0ffdadd61b4d357fb3d
yavuzkocar <87602474+yavuzkocar@users.noreply.github.com>
2021-10-29 14:57:09 +0300

    if-else-if

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+             //if (time >= 6 && time < 11)
+             //Console.WriteLine("günaydın");
+            
+             //else if(time<= 18)
+             //Console.WriteLine("iyi günler");
+            
+            // else
+              //Console.WriteLine("iyi gECELER");
+               
+               string sonuç = time>= 6 && time < 11 ? "günaydın" : time <=18 ? "iyi günler" : "iyi geceler" ;
+               Console.WriteLine(sonuç); 
+
+        }
+    }
+}
